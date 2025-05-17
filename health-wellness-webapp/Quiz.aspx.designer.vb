fa7811b58617f0e3f2744405fcd984e117ff04af
Justin <justinchen2@outlook.com>
2025-05-17 14:44:12 +1200

    chore: quiz page complete

--- health-wellness-webapp/Quiz.aspx.designer.vb
+++ health-wellness-webapp/Quiz.aspx.designer.vb
@@ -201,4 +201,22 @@ Partial Public Class Quiz
     '''To modify move field declaration from designer file to code-behind file.
     '''</remarks>
     Protected WithEvents RadioButtonList20 As Global.System.Web.UI.WebControls.RadioButtonList
+
+    '''<summary>
+    '''btnSubmit control.
+    '''</summary>
+    '''<remarks>
+    '''Auto-generated field.
+    '''To modify move field declaration from designer file to code-behind file.
+    '''</remarks>
+    Protected WithEvents btnSubmit As Global.System.Web.UI.WebControls.Button
+
+    '''<summary>
+    '''lblResult control.
+    '''</summary>
+    '''<remarks>
+    '''Auto-generated field.
+    '''To modify move field declaration from designer file to code-behind file.
+    '''</remarks>
+    Protected WithEvents lblResult As Global.System.Web.UI.WebControls.Label
 End Class
