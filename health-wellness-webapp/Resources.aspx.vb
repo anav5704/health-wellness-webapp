b50e4488d2b45ed2196f333327c583e9ccc5d32c
Aryan <aryanzsingh444@gmail.com>
2025-05-18 16:57:51 +1200

    chore: add resource page

--- health-wellness-webapp/Resources.aspx.vb
+++ health-wellness-webapp/Resources.aspx.vb
@@ -7,19 +7,4 @@
             Response.Redirect("Login.aspx?ReturnUrl=" & Server.UrlEncode(Request.RawUrl))
         End If
     End Sub
-
-    Protected Sub btnDownload_Click(ByVal sender As Object, ByVal e As EventArgs)
-        Dim fileName As String = "Healthy Habits & Routines.pdf"
-        Dim filePath As String = Server.MapPath("~/App_Data/app_data/" & fileName)
-
-        If System.IO.File.Exists(filePath) Then
-            Response.Clear()
-            Response.ContentType = "application/pdf"
-            Response.AppendHeader("Content-Disposition", "attachment; filename=" & Server.UrlEncode(fileName))
-            Response.TransmitFile(filePath)
-            Response.End()
-        Else
-            ClientScript.RegisterStartupScript(Me.GetType(), "FileNotFound", "alert('File not found.');", True)
-        End If
-    End Sub
 End Class
