d0de25260405b7d3e8e8663e8705e8197e5ae7d0
anav5704 <anavkki@gmail.com>
2025-05-23 17:14:35 +1200

    fix: conditionally render auth button

--- health-wellness-webapp/Site.Master.vb
+++ health-wellness-webapp/Site.Master.vb
@@ -2,27 +2,7 @@
     Inherits System.Web.UI.MasterPage
 
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
-        If Not IsPostBack Then
-            RenderAuthLinks()
-        End If
-    End Sub
 
-    Private Sub RenderAuthLinks()
-        If Session("User_ID") IsNot Nothing Then
-            Dim logoutLink As New HyperLink()
-            logoutLink.NavigateUrl = "Logout.aspx"
-            logoutLink.CssClass = "primary-link"
-            logoutLink.ID = "auth-links"
-            logoutLink.Text = "Log Out"
-            phAuthLinks.Controls.Add(logoutLink)
-        Else
-            Dim signupLink As New HyperLink()
-            signupLink.NavigateUrl = "Signup.aspx"
-            signupLink.CssClass = "primary-link"
-            signupLink.ID = "auth-links"
-            signupLink.Text = "Create Account"
-            phAuthLinks.Controls.Add(signupLink)
-        End If
     End Sub
 
 End Class
