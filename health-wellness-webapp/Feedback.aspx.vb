e928fa355d9f4ccd42f432c24d0219a9f641e0f3
Vishant Kumar <163496021+v-i-s-h-a-n-t@users.noreply.github.com>
2025-05-23 16:21:13 +1200

    chore: complete feedback page

--- health-wellness-webapp/Feedback.aspx.vb
+++ health-wellness-webapp/Feedback.aspx.vb
@@ -7,4 +7,20 @@
         End If
     End Sub
 
+    Protected Sub btnFeedback_Click(sender As Object, e As EventArgs) Handles btnFeedback.Click
+        Dim message As String = txtMessage.Text.Trim()
+
+        If message = "" Then
+            lblConfirmation.Text = "Please enter feedback message"
+            Exit Sub
+        End If
+
+        Dim userID As Integer = Convert.ToInt32(Session("User_ID"))
+
+        adsFeedback.InsertParameters("User_Id").DefaultValue = userID.ToString()
+        adsFeedback.InsertParameters("Feedback_Message").DefaultValue = message
+
+        adsFeedback.Insert()
+        lblConfirmation.Text = "Thank you for sending your feedback"
+    End Sub
 End Class
