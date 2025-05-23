95a8affcf6951900f2e7b32f075d7de4cf28b2af
anav5704 <anavkki@gmail.com>
2025-05-24 11:21:54 +1200

    feat: add user dashboard

--- health-wellness-webapp/Dashboard.aspx
+++ health-wellness-webapp/Dashboard.aspx
@@ -1,5 +1,34 @@
-﻿<%@ Page Title="" Language="vb" AutoEventWireup="false" MasterPageFile="~/Site.Master" CodeBehind="Dashboard.aspx.vb" Inherits="health_wellness_webapp.Dashboard" %>
+﻿<%@ Page Title="" ClientIDMode="Static" Language="vb" AutoEventWireup="false" MasterPageFile="~/Site.Master" CodeBehind="Dashboard.aspx.vb" Inherits="health_wellness_webapp.Dashboard" %>
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+       <h1><asp:Label ID="lblUserRole" Text="" runat="server"></asp:Label> Dashboard</h1>
+
+    <p class="userInfo">
+        You are logged in as <asp:Label ID="lblUserName" Text="" runat="server"></asp:Label>
+        <asp:HyperLink ID="hlLogout" runat="server" NavigateUrl="Logout.aspx" Text="Log Out" />
+    </p>
+
+    <asp:GridView ID="gvBookings" runat="server" AutoGenerateColumns="False"  DataSourceID="adsBookings" DataKeyNames="Booking_Id">
+        <Columns>
+            <asp:BoundField DataField="UserName" HeaderText="User" />
+            <asp:BoundField DataField="Therapist_Name" HeaderText="Therapist" />
+            <asp:BoundField DataField="Booking_Time" HeaderText="Time" />
+            <asp:TemplateField HeaderText="Action">
+                <ItemTemplate>
+                    <asp:HyperLink ID="hlEdit" runat="server" NavigateUrl='<%# Eval("Booking_Id", "EditBooking.aspx?id={0}") %>' Text="Edit" />
+                </ItemTemplate>
+            </asp:TemplateField>
+        </Columns>
+    </asp:GridView>
+
+    <asp:AccessDataSource ID="adsBookings" runat="server" DataFile="~/App_Data/Webapp.accdb">
+    </asp:AccessDataSource>
+
+    <asp:AccessDataSource ID="adsUser" runat="server" DataFile="~/App_Data/Webapp.accdb" SelectCommand="SELECT [User_Fname] & ' ' & [User_Lname] AS User_Name FROM [User] WHERE [User_Id] = ?">
+        <SelectParameters>
+            <asp:Parameter Name="User_Id" Type="String" />
+        </SelectParameters>
+    </asp:AccessDataSource>
+
 </asp:Content>
--- health-wellness-webapp/Dashboard.aspx.vb
+++ health-wellness-webapp/Dashboard.aspx.vb
@@ -1,30 +1,46 @@
-﻿Public Class Dashboard
+﻿Imports System.Data.OleDb
+
+Public Class Dashboard
     Inherits System.Web.UI.Page
 
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
         If Not IsPostBack Then
-            If Session("User_ID") Is Nothing Then
-                Response.Redirect("Login.aspx")
-                Exit Sub
+            If Session("User_Role") IsNot Nothing AndAlso Session("User_Role").ToString() = "Admin" Then
+                adsBookings.SelectCommand = "
+                    SELECT 
+                        Booking.Booking_Id, 
+                        Therapist.Therapist_Name, 
+                        [User].User_Fname & ' ' & [User].User_Lname AS UserName, 
+                        FORMAT(Booking.Booking_Time, 'MMM dd, yyyy hh:mm') AS Booking_Time 
+                    FROM 
+                        (Booking 
+                        INNER JOIN Therapist ON Booking.Therapist_Id = Therapist.Therapist_Id)
+                        INNER JOIN [User] ON Booking.User_Id = [User].User_Id"
+
+            ElseIf Session("User_ID") IsNot Nothing Then
+                adsBookings.SelectCommand = "
+                SELECT 
+                    Booking.Booking_Id, 
+                    Therapist.Therapist_Name, 
+                    [User].User_Fname & ' ' & [User].User_Lname AS UserName, 
+                    FORMAT(Booking.Booking_Time, 'MMM dd, yyyy hh:mm') AS Booking_Time 
+                FROM 
+                    (Booking 
+                    INNER JOIN Therapist ON Booking.Therapist_Id = Therapist.Therapist_Id)
+                    INNER JOIN [User] ON Booking.User_Id = [User].User_Id 
+                WHERE 
+                    Booking.User_Id = ?"
+
+                adsBookings.SelectParameters.Add("User_Id", Session("User_ID").ToString())
+            Else
+                Response.Redirect("~/Login.aspx")
             End If
 
-            If Session("User_Role") = "Admin" Then
-                LoadAllBookings()
-            Else
-                LoadUserBookings(Session("User_ID").ToString())
-            End If
+            adsUser.SelectParameters("User_Id").DefaultValue = Session("User_ID").ToString()
+            Dim dv As Data.DataView = CType(adsUser.Select(DataSourceSelectArguments.Empty), Data.DataView)
+
+            lblUserName.Text = dv(0)("User_Name").ToString() & "."
+            lblUserRole.Text = Session("User_Role").ToString()
         End If
     End Sub
-
-    Private Sub LoadAllBookings()
-        'gvBookings.DataSourceID = "adsAllBookings"
-        'gvBookings.DataBind()
-    End Sub
-
-    Private Sub LoadUserBookings(userId As String)
-        'adsUserBookings.SelectParameters("User_ID").DefaultValue = userId
-        'gvBookings.DataSourceID = "adsUserBookings"
-        'gvBookings.DataBind()
-    End Sub
-
 End Class
